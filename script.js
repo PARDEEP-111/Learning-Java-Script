1c795a6b562e562439c8b941f7fda1d04821f61a
PARDEEP 111 <pardeepsingh091209>
2025-03-24 09:47:08 +0530

    completed selcet by id,classes,query and many more

---
+++ script.js
@@ -0,0 +1,12 @@
+// let box = document.getElementsByClassName("box")
+//         box[2].style.backgroundColor = "red"
+
+// let red =document.getElementById("red")
+// red.style.backgroundColor="red"
+
+// document.querySelector(".box").style.backgroundColor="red"
+
+document.querySelectorAll(".box").forEach(e=>{
+
+    e.style.backgroundColor="red"
+})
